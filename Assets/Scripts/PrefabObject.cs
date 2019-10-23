d4b42629e4ae1be8b62b49ca132bf34b7b7cfc8a
nguyen huy Hoang <hoang2410@gmail.com>
2019-10-23 16:29:07 +0700

    add assets

---
+++ Assets/Scripts/PrefabObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabObject : MonoBehaviour {
+  // Start is called before the first frame update
+  void Start() {
+    Debug.Log("=========Last prefab loaded in " + (Time.realtimeSinceStartup - TestAUP.startLoadTime));
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+}
---
+++ Assets/Scripts/TestAUP.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAUP : MonoBehaviour {
+  // Start is called before the first frame update
+  public Transform parentTransform;
+
+  public static double startLoadTime;
+
+  void Start() {
+    // Invoke("LoadAssets", 1.0f);
+
+    Invoke("LoadAsyncAssets", 1.0f);
+  }
+
+  // Update is called once per frame
+  void LoadAssets() {
+    double t = Time.realtimeSinceStartup;
+    startLoadTime = Time.realtimeSinceStartup;
+
+    PerformanceTuner.Mode = PerformanceTuner.PerformanceMode.Loading;
+    for (int i = 1; i <= 20; i++) {
+      GameObject go = Instantiate(Resources.Load("Prefabs/build_" + i)) as GameObject;
+      go.transform.parent = parentTransform;
+    }
+
+    PerformanceTuner.Mode = PerformanceTuner.PerformanceMode.Gameplay;
+    Debug.Log("-----LoadAssets done in " + (Time.realtimeSinceStartup - t));
+  }
+
+  void LoadAsyncAssets() {
+    startLoadTime = Time.realtimeSinceStartup;
+    StartCoroutine(LoadAsyncAssetsCoroutine());
+  }
+
+  IEnumerator LoadAsyncAssetsCoroutine() {
+    // PerformanceTuner.Mode = PerformanceTuner.PerformanceMode.Loading;
+    double t = Time.realtimeSinceStartup;
+    for (int i = 1; i <= 20; i++) {
+      ResourceRequest request = Resources.LoadAsync<GameObject>("Prefabs/build_" + i);
+      while (!request.isDone) {
+        yield return 0;
+      }
+
+      GameObject go = Instantiate(request.asset) as GameObject;
+      go.transform.parent = parentTransform;
+      // yield return null;
+    }
+
+    // PerformanceTuner.Mode = PerformanceTuner.PerformanceMode.Gameplay;
+    Debug.Log("-----LoadAsyncAssets done in " + (Time.realtimeSinceStartup - t));
+  }
+}
